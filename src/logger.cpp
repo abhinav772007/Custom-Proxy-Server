ec735b905eab2e3f93d7e7042e8ecb7e0f250e2a
abhinav772007 <abhinavs1256@gmail.com>
2026-01-03 10:51:23 +0530

    update rotatelog()

--- src/logger.cpp
+++ src/logger.cpp
@@ -5,14 +5,14 @@
 #include <fstream>
 #include <iomanip>
 #include <sstream>
+#include<vector>
+#ifdef _WIN32
+#include <direct.h>
+#endif
 using namespace std;
-
 #include<iostream>
-
-
-static string logfile="../logs/proxy.log";
-
-static const size_t maxsize=200;
+static string logfile="logs/proxy.log";
+static const size_t maxsize=4096;
 
 static string currenttime(){
     time_t now=time(nullptr);
@@ -22,24 +22,40 @@ static string currenttime(){
     return out.str();
     
 }
-static void rotatelog(){
-    struct stat st;
-    if(stat(logfile.c_str(),&st)==0){
-        if(st.st_size>=maxsize){
-            rename(logfile.c_str(),"../logs/proxy.log.old");
-        }
+static void ensure_log_dir() {
+    #ifdef _WIN32
+        _mkdir("logs");
+    #else
+        mkdir("logs", 0755);
+    #endif
     }
+    
+static void rotatelog(){
+    ifstream in(logfile,ios::binary | ios::ate);
+    if(!in.is_open())return ;
+    size_t size=in.tellg();
+    if(size<=maxsize){
+        in.close();
+        return;
+    }
+    size_t present=maxsize;
+    in.seekg(size-present);
+    vector<char> buffer(present);
+    in.read(buffer.data(),present);
+    in.close();
+
+    ofstream out(logfile,ios::binary | ios::trunc);
+    out.write(buffer.data(),present);
+    out.close();
 }
 
 void logreq(const std::string &clientadd,const std::string &serveradd,const std::string &requestline,const std::string &action,int statuscode,size_t bytes_transferred){
+    ensure_log_dir();
     rotatelog();
     ofstream log(logfile,std::ios::app);
     if(!log.is_open()){
+        cerr<<"[Logger] failed to open\n";
         return ;
     }
     log<<currenttime()<<" | "<<clientadd<<" | "<<serveradd<<" | "<<requestline<<" | "<<action<<" | "<<statuscode<<" | "<<bytes_transferred<<"\n";
 }
-
-
-
-
