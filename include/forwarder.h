1d3348d1ebaccce308f27aeafe7d2f13336f3d59
abhinav772007 <abhinavs1256@gmail.com>
2026-01-05 10:52:48 +0530

    add header files

--- include/forwarder.h
+++ include/forwarder.h
@@ -2,6 +2,8 @@
 #define FORWARDER_H
 #include "parser.h"
 #include <string>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
 bool forward_request(SOCKET client_sock, const HttpRequest &req);// true on success and false on failures
 bool handle_connect(SOCKET client_sock,const HttpRequest &req);
